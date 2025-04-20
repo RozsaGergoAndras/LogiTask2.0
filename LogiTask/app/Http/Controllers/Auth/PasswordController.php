5e9e6e8d97c1e9ca271ab365a70fbbac497fcfa6
MarkG556 <77093844+MarkG556@users.noreply.github.com>
2025-04-20 11:15:35 +0200

    fix

--- LogiTask/app/Http/Controllers/Auth/PasswordController.php
+++ LogiTask/app/Http/Controllers/Auth/PasswordController.php
@@ -34,7 +34,7 @@ class PasswordController extends Controller
             'password' => ['required', Password::defaults(), 'confirmed'],
         ]);
 
-        $request->user()->update([
+        auth('sanctum')->user()->update([
             'password' => Hash::make($validated['password']),
         ]);
 
